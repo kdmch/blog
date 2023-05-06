14e511442995fd989e830de43d3da2b1ef1ea65c
kdmch <mitori.kadomachi@gmail.com>
2023-05-07 08:41:00 +0900

    about updated

---
+++ components/Popup.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-dialog v-model="dialog" max-width="500">
+    <v-card>
+      <v-card-title>{{ title }}</v-card-title>
+      <v-card-text>
+        {{ message }}
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="primary" text @click="dialog = false">閉じる</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    message: String
+  },
+  data() {
+    return {
+      dialog: true
+    }
+  }
+}
+</script>
--- pages/about.vue
+++ pages/about.vue
@@ -1,39 +1,68 @@
 <template>
   <v-app>
+    <Popup v-if="show" title="" message="copied!" />
     <div id="twbg" />
     <div class="main">
       <v-row>
-        <img class="twicon" src="/icons/melmitori.png">
+        <a href="https://twitter.com/mitorime" target="_blank" rel="noopener noreferrer">
+          <img class="twicon" src="/icons/melmitori.png">
+        </a>
         <v-col class="tweet">
-          <v-row class="nowrap">
-            <div class="username">みとり</div>
-            <div class="screenname">&nbsp;@mitorime&nbsp;</div>
-            <div class="twdate">·&nbsp;2023/05/01</div>
-          </v-row>
+          <a href="https://twitter.com/mitorime" target="_blank" rel="noopener noreferrer">
+            <v-row class="nowrap">
+              <div class="username">みとり</div>
+              <div class="screenname">&nbsp;@mitorime&nbsp;</div>
+              <div class="twdate">·&nbsp;2023/05/07</div>
+            </v-row>
+          </a>
           <v-row>
             <div class="twmain">
               <div><v-icon small color="#1995e0" class="icon">mdi-twitter</v-icon>ごちゃまぜのアカウントです。</div>
               <div>アグネスタキオン/言語学/ボカロ/プログラミング/作曲/絵/コスプレ(する方)/道路/ピノキオピー/原神/凋叶棕/音ゲー/短歌/デザイン/いろいろ</div>
               <br>
-              <div><v-icon small color="#1995e0" class="icon">mdi-account-eye</v-icon>閲覧垢です→<a href="https://twitter.com/laykatase" target="_blank" rel="noopener noreferrer">@laykatase</a></div>
+              <div><v-icon small color="#1995e0" class="icon">mdi-antenna</v-icon>閲覧垢です→<a href="https://twitter.com/laykatase" target="_blank" rel="noopener noreferrer" class="twlink">@laykatase</a></div>
               <div>閲覧垢でいいねしてこっちでRT、みたいなのをよくやります   お気になさらず</div>
               <br>
-              <div><v-icon small color="#1995e0" class="icon">mdi-account-arrow-right</v-icon>疎開先→<a href="https://fedibird.com/@mitori" target="_blank" rel="noopener noreferrer">@mitori (fedibird)</a></div>
-              <div>自鯖も考えたけど、単に技術不足なのと、維持費、サーバが死んだら私も死ぬ、などからfedibirdに疎開してます。</div>
+              <div><v-icon small color="#1995e0" class="icon">mdi-mastodon</v-icon>疎開先→<a href="https://fedibird.com/@mitori" target="_blank" rel="noopener noreferrer" class="twlink">@mitori (Fedibird)</a></div>
+              <div>自鯖も考えたけど、単に技術不足なのと、維持費、サーバが死んだら私も死ぬ、などの理由からFedibirdに疎開してます。</div>
               <div>ほぼTwitterにいるけど……</div>
               <br>
+              <div><v-icon small color="#1995e0" class="icon">mdi-lock</v-icon>縮小→<a href="https://twitter.com/zilitz" target="_blank" rel="noopener noreferrer" class="twlink">@zilitz</a></div>
+              <div>FFなら高確率で通ります。思考の吐き溜め。自己責任で。</div>
+              <br>
+              <div><v-icon small color="#1995e0" class="icon">mdi-link-variant</v-icon>その他リンク</div>
+              <div><a href="https://mitori.me" target="_blank" rel="noopener noreferrer" class="twlink"><v-icon small color="#1995e0" class="icon">mdi-web</v-icon>blog: ミトリメ(mitori.me)&nbsp;<v-icon small color="#1995e0" class="icon">mdi-open-in-new</v-icon></a></div>
+              <div><a href="mailto:mail@mitori.me" target="_blank" rel="noopener noreferrer" class="twlink"><v-icon small color="#1995e0" class="icon">mdi-email</v-icon>email: mailあっとmitori.me&nbsp;<v-icon small color="#1995e0" class="icon">mdi-open-in-new</v-icon></a></div>
+              <div><a href="https://github.com/kdmch" target="_blank" rel="noopener noreferrer" class="twlink"><v-icon small color="#1995e0" class="icon">mdi-github</v-icon>GitHub: kdmch&nbsp;<v-icon small color="#1995e0" class="icon">mdi-open-in-new</v-icon></a></div>
+              <div><a href="https://keybase.io/mitori" target="_blank" rel="noopener noreferrer" class="twlink"><v-icon small color="#1995e0" class="icon">mdi-key-variant</v-icon>Keybase: mitori&nbsp;<v-icon small color="#1995e0" class="icon">mdi-open-in-new</v-icon></a></div>
+              <div class="twlink" @click="copyTag('mitori#5200')"><v-icon small color="#1995e0" class="icon">mdi-chat</v-icon>Discord: mitori#5200&nbsp;<v-icon small color="#1995e0" class="icon">mdi-content-copy</v-icon></div>
+              <div class="twlink" @click="copyTag('mitorik')"><v-icon small color="#1995e0" class="icon">mdi-virtual-reality</v-icon>VRChat: mitorik&nbsp;<v-icon small color="#1995e0" class="icon">mdi-content-copy</v-icon></div>
+              <div class="twlink" @click="copyTag('858144259')"><v-icon small color="#1995e0" class="icon">mdi-star-four-points</v-icon>原神: 858144259&nbsp;<v-icon small color="#1995e0" class="icon">mdi-content-copy</v-icon></div>
+              <br>
               <div><v-icon small color="#1995e0" class="icon">mdi-brush</v-icon>絵やら曲やらをたまーに上げますが</div>
-              <div>著作権ほぼ放棄でやってるので、無断転載も自作発言もそのまま複製して売るのも完全OK お前のものです 自由にしてください</div>
+              <div>著作権ほぼ放棄でやってるので、素材にするのもAI学習も無断転載も自作発言もそのまま複製して売るのも完全OK！あなたのものです。自由にしてください！</div>
+              <v-row class="cap-container">
+                <a href="https://elaws.e-gov.go.jp/document?lawid=345AC0000000048" target="_blank" rel="noopener noreferrer">
+                  <div class="cap-normal shadow">
+                    <div><v-icon small color="#c10600" class="icon">mdi-alert</v-icon><b>&nbsp;ただし、特定個人・団体やすべての宗教的・政治的思想を称揚または攻撃する目的では使用しないでください。</b></div>
+                    <div>また、ごく稀にコスプレ写真が上がりますが、<b>こちらは転載不可になります。著作権を侵害しない範囲での私的利用に留めてください。</b></div>
+                    <div>上記を逸脱した使用に関しては、発見次第しかるべき措置をとります。</div>
+                  </div>
+                </a>
+              </v-row>
+              <div>クオリティがアレなものしか作れてないけど、こういうのはしっかり言っておくべきですね……</div>
               <br>
             </div>
           </v-row>
-          <v-row justify="space-between">
-            <v-icon small color="#526472" class="icon">mdi-comment-outline</v-icon>
-            <v-icon small color="#526472" class="icon">mdi-repeat-variant</v-icon>
-            <v-icon small color="#526472" class="icon">mdi-heart-outline</v-icon>
-            <v-icon small color="#526472" class="icon">mdi-poll</v-icon>
-            <v-icon small color="#526472" class="icon">mdi-export-variant</v-icon>
-          </v-row>
+          <a href="https://twitter.com/mitorime" target="_blank" rel="noopener noreferrer">
+            <v-row justify="space-between">
+              <v-icon small color="#526472" class="icon">mdi-comment-outline</v-icon>
+              <v-icon small color="#526472" class="icon">mdi-repeat-variant</v-icon>
+              <v-icon small color="#526472" class="icon">mdi-heart-outline</v-icon>
+              <v-icon small color="#526472" class="icon">mdi-poll</v-icon>
+              <v-icon small color="#526472" class="icon">mdi-export-variant</v-icon>
+            </v-row>
+          </a>
         </v-col>
       </v-row>
     </div>
@@ -50,7 +79,8 @@ export default {
         type: 'article',
         url: 'https://mitori.me/about',
         image: 'https://raw.githubusercontent.com/kdmch/blog/master/static/img/pin.png'
-      }
+      },
+      show: false
     }
   },
   head () {
@@ -65,6 +95,12 @@ export default {
         { hid: 'og:image', property: 'og:image', content: this.meta.image },
         { hid: 'twitter:card', name: 'twitter:card', content: 'summary' }
       ]
+    }
+  },
+  methods: {
+    copyTag (tagstr) {
+      navigator.clipboard.writeText(tagstr)
+      // this.show = true
     }
   }
 }
@@ -85,31 +121,67 @@ export default {
   display: flex;
   flex-flow: column nowrap;
   align-items: center;
-  padding: 100px 32px;}
-    .tweet {
-      max-width: 480px;
-      color: #0f1419;}
-      .username {
-        font-weight: bold;
-      }
-      .screenname {
-        color: #536471;
-      }
-      .twdate {
-        color: #536471;
-      }
-      .twmain {
-        display: flex;
-        flex-flow: column nowrap;
-        gap: 0px;
-        padding: 8px 2px;
-      }
+  padding: 100px 32px 100px 24px;
+}
+.tweet {
+  max-width: 500px;
+  color: #0f1419;
+}
+.username {
+  color: #0f1419;
+  font-weight: bold;
+}
+.screenname {
+  color: #536471;
+}
+.twdate {
+  color: #536471;
+}
+.twmain {
+  display: flex;
+  flex-flow: column nowrap;
+  gap: 0px;
+  padding: 8px 2px;
+}
 .twicon {
   padding: 5px;
   width: 64px;
   height: 64px;
   border-radius: 50%;
 }
+
+.cap-container {
+  margin: 6px;
+  gap: 12px;
+}
+.cap-normal {
+  width: 100%;
+  margin: 8px;
+  padding: 16px;
+  color: #0f1419;
+  background-color: #fff;
+  border-radius: 12px;
+}
+.cap-check {
+  width: 100%;
+  padding: 24px;
+  background-color: #7dffaba3;
+  border-radius: 12px;
+}
+.cap-x {
+  width: 100%;
+  padding: 12px;
+  background-color: #ff7d99a3;
+  border-radius: 12px;
+}
+.shadow {
+  box-shadow: 0px 0px 16px #ccc, 0px 0px 24px -6px #f8f8ff inset;
+  transition: .6s;
+}
+.shadow:hover {
+  box-shadow: 0px 0px 16px #888, 0px 0px 24px -6px #f8f8ff inset;
+}
+
 .nowrap {
   flex-wrap: nowrap;
 }
@@ -119,11 +191,14 @@ export default {
   margin-top: -4px;
 }
 
-a {
+.twlink {
   color: #1995e0;
   text-decoration: none;
 }
-a:hover {
+.twlink:hover {
   text-decoration-line: underline;
+}
+a {
+  text-decoration: none;
 }
 </style>
