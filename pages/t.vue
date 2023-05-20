60a11231e96b15671115e532e492f797d37149f9
kdmch <mitori.kadomachi@gmail.com>
2023-05-20 16:33:37 +0900

    created shortlink

---
+++ pages/t.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <div id="main">
+    </div>
+  </v-app>
+</template>
+
+<script>
+export default {
+  mounted() {
+    window.location.href = 'https://twitter.com/mitorime'
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
